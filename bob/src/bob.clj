56b5e76c1be1dfdb37ca6cba70cb9b28176a833c
Andre Anastacio <andreluisanastacio@gmail.com>
2016-03-16 19:59:24 -0300

    [WIP] Solve bob

--- bob/src/bob.clj
+++ bob/src/bob.clj
@@ -1,4 +1,13 @@
 (ns bob
   (:require [clojure.string :as str]))
 
-(defn response-for [phrase])
+(defn question? [word]
+  (str/ends-with? word "?"))
+
+(defn shout? [word]
+  (= (str/upper-case word) word))
+
+(defn response-for [phrase]
+  (case
+    (question? phrase) "Sure.")
+    (shout? phrase) "Whoa, chill out!")
