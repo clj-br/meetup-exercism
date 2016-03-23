a83e2d857695058d1869d114a48bdf3756cee7b8
Vinícius Soares <squimer@gmail.com>
2016-03-23 20:36:46 -0300

    Solve rna-transcription.

--- rna-transcription/src/rna_transcription.clj
+++ rna-transcription/src/rna_transcription.clj
@@ -1,4 +1,12 @@
 (ns rna-transcription)
 
+(defn dna->rna [char]
+  (cond
+    (= char \C) "G"
+    (= char \G) "C"
+    (= char \A) "U"
+    (= char \T) "A"
+    :else (throw (AssertionError.))))
+
 (defn to-rna [strand]
-  "C")
+  (clojure.string/join (map dna->rna strand)))
