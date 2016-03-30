9c9403e0db9873251485c0691abc46ac5fcbd702
Andre Anastacio <andreluisanastacio@gmail.com>
2016-03-30 19:42:25 -0300

    Continue word count exercise

--- word-count/src/word_count.clj
+++ word-count/src/word_count.clj
@@ -1,7 +1,13 @@
 (ns word-count)
 
+(defn split-phrase [phrase]
+  (clojure.string/split (clojure.string/lower-case phrase) #" "))
+
 (defn word-count [phrase]
  (reduce (fn [result word]
            (update-in result [word] (fnil inc 0)))
    {}
-   (clojure.string/split phrase #" ")))
+         (split-phrase phrase)))
+
+
+
