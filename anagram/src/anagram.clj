6c2582aaf8457557845e88e972c5512b3dca7948
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2016-04-06 19:53:25 -0300

    WIP

--- anagram/src/anagram.clj
+++ anagram/src/anagram.clj
@@ -1,11 +1,14 @@
 (ns anagram
   (:require [clojure.string :as string]))
 
-
+(defn- -sort [word]
+  (-> word .toLowerCase sort string/join))
 
 (defn anagrams-for [word possibilities]
-  (let [anagram (string/join (sort word))
-        ordered-possibilities (map #(string/join (sort %)) possibilities)]
-    (filter (fn [w] (= anagram w)) ordered-possibilities)))
+  (let [anagram (-sort word)]
+    (filter (fn [poss]
+              (println anagram poss (-sort poss))
+              (and (not= word poss)
+                   (= anagram (-sort poss)))) possibilities)))
 
 
