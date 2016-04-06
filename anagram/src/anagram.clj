c70d4c623e86e90250bcecedd8782c9c604a5b4e
Andre Anastacio <andreluisanastacio@gmail.com>
2016-04-06 19:24:47 -0300

    Continue anagram exercise

--- anagram/src/anagram.clj
+++ anagram/src/anagram.clj
@@ -1,3 +1,10 @@
 (ns anagram)
 
-(defn anagrams-for [foo bar])
+(defn anagrams-for [word possibilities]
+  (let [anagram (clojure.string/reverse word)]
+    (filter
+      (fn [w]
+        (= anagram w))
+      possibilities)))
+
+
