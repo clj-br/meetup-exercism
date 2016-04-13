bc0447050bb368cb279119a56b68b4e5def6d623
Tomás Augusto Müller <tomas.am@gmail.com>
2016-04-13 20:15:02 -0300

    Solve beer song.

--- beer-song/src/beer_song.clj
+++ beer-song/src/beer_song.clj
@@ -1,25 +1,28 @@
 (ns beer-song
-  (:require [clojure.string :refer [lower-case]]))
+  (:require [clojure.string :as string]))
 
 (defn bottles [n]
-  (cond
-    (= n 1) "1 bottle"
-    (= n 0) "No more bottles"
-    :else (str n " bottles")))
+  (case n
+    0 "No more bottles"
+    1 "1 bottle"
+    (str n " bottles")))
+
+(defn first-phrase [n]
+  (str (bottles n) " of beer on the wall, " (string/lower-case (bottles n)) " of beer.\n"))
 
 (defn second-phrase [n]
-  (cond
-    (= n 1) "Take one down and pass it around, 1 bottle of beer on the wall.\n"
-    (> n 1) (str "Take one down and pass it around, " n " bottles of beer on the wall.\n")
-    (= n 0) "Take it down and pass it around, no more bottles of beer on the wall.\n"
-    :else "Go to the store and buy some more, 99 bottles of beer on the wall.\n"))
+  (case n
+    0 "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+    1 "Take it down and pass it around, no more bottles of beer on the wall.\n"
+    (str "Take one down and pass it around, " (bottles (dec n)) " of beer on the wall.\n")))
 
 (defn verse [n]
-  (str (bottles n) " of beer on the wall, " (lower-case (bottles n)) " of beer.\n"
-       (second-phrase (dec n))))
+  (str (first-phrase n)
+       (second-phrase n)))
 
 (defn sing
   ([start] (sing start 0))
   ([start end]
    (->> (range start (dec end) -1)
-        (map verse))))
+        (map verse)
+        (string/join "\n"))))
