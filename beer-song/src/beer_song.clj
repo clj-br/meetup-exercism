7ce5f5d77dc6d2c38b369284b45f83af40f5513c
Tomás Augusto Müller <tomas.am@gmail.com>
2016-04-13 19:32:00 -0300

    Continue beer song exercism.

--- beer-song/src/beer_song.clj
+++ beer-song/src/beer_song.clj
@@ -3,11 +3,17 @@
 (defn bottles [n]
   (cond
     (= n 1) "1 bottle"
-    (= n 0) "no more"
+    (= n 0) "no more bottles"
     :else (str n " bottles")))
 
+(defn second-phrase [n]
+  (cond
+    (= n 1) "Take it down and pass it around, no more bottles of beer on the wall.\n\n"
+    (> n 1) (str "Take one down and pass it around, " n " bottles of beer on the wall.\n")
+    :else "Go to the store and buy some more, 99 bottles of beer on the wall.\n"))
+
 (defn verse [n]
-  (str n " bottles of beer on the wall, " n " bottles of beer.\n"
-       "Take one down and pass it around, " (bottles (dec n)) " of beer on the wall.\n"))
+  (str (bottles n) " of beer on the wall, " (bottles n) " of beer.\n"
+       (second-phrase (dec n))))
 
 (defn sing [n])
