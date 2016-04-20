40ae2ba7ece301f17ea97cec79d5dd620db26b4b
Tomás Augusto Müller <tomas.am@gmail.com>
2016-04-20 19:36:58 -0300

    Refactor nucleotide count exercise.

--- nucleotide-count/src/nucleotide_count.clj
+++ nucleotide-count/src/nucleotide_count.clj
@@ -1,10 +1,10 @@
 (ns nucleotide-count
   (:refer-clojure :exclude [count]))
 
-(defn count [char dna]
-  {:pre [(contains? #{\G \A \C \T} char)]}
-  (-> (frequencies dna)
-      (get char 0)))
-
 (defn nucleotide-counts [dna]
-  (reduce (fn [coll item] (update coll item inc)) {\A 0 \T 0 \C 0 \G 0} dna))
+  (reduce
+    (fn [m nucleotide] (update m nucleotide inc)) {\A 0 \T 0 \C 0 \G 0} dna))
+
+(defn count [char dna]
+  {:pre [(#{\G \A \C \T} char)]}
+  (get (nucleotide-counts dna) char))
