5369788b9acef15734fd96714ac451b143aa94a8
Vinícius Soares <squimer@gmail.com>
2016-04-20 20:06:22 -0300

    Solve hamming exercise

--- hamming/src/hamming.clj
+++ hamming/src/hamming.clj
@@ -1,4 +1,8 @@
 (ns hamming)
 
 (defn distance [first last]
-  (if (not= (count first) (count last)) nil 0))
+  (if (not= (count first) (count last))
+    nil
+    (do
+      (let [tuples (map vector first last)]
+        (reduce (fn [count [a b]] (if (= a b) count (inc count))) 0 tuples)))))
