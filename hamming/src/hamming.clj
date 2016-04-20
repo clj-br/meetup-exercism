26057cd348f6b42e80c9bb5c47bc0e04d1c4df73
Vinícius Soares <squimer@gmail.com>
2016-04-20 20:19:21 -0300

    Refactor hamming

--- hamming/src/hamming.clj
+++ hamming/src/hamming.clj
@@ -1,8 +1,9 @@
 (ns hamming)
 
-(defn distance [first last]
-  (if (not= (count first) (count last))
-    nil
-    (do
-      (let [tuples (map vector first last)]
-        (reduce (fn [count [a b]] (if (= a b) count (inc count))) 0 tuples)))))
+(defn count-differences [count [a b]]
+  (if (= a b) count (inc count)))
+
+(defn distance [left right]
+  (when (= (count left) (count right))
+    (let [tuples (map vector left right)]
+      (reduce count-differences 0 tuples))))
