a6ea849c6eefb39aedbcf9baaa634f3139e99fc0
Tomás Augusto Müller <tomas.am@gmail.com>
2016-04-20 19:30:36 -0300

    Solve nucleotide count exercise.

--- nucleotide-count/src/nucleotide_count.clj
+++ nucleotide-count/src/nucleotide_count.clj
@@ -2,9 +2,9 @@
   (:refer-clojure :exclude [count]))
 
 (defn count [char dna]
-  (->
-    (frequencies dna)
-    (get char)))
+  {:pre [(contains? #{\G \A \C \T} char)]}
+  (-> (frequencies dna)
+      (get char 0)))
 
 (defn nucleotide-counts [dna]
   (reduce (fn [coll item] (update coll item inc)) {\A 0 \T 0 \C 0 \G 0} dna))
