91ac24fd54618ff787d04aa6a4e6d311a9269ca7
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2016-04-27 19:37:37 -0300

    All tests pass

--- phone-number/src/phone_number.clj
+++ phone-number/src/phone_number.clj
@@ -2,15 +2,21 @@
   (:require [clojure.string :as string]))
 
 (defn number [n]
-  (let [clean-number (string/join (re-seq #"\d" n))]
+  (let [clean-number (string/join (re-seq #"\d" n))
+        invalid "0000000000"]
     (cond
       (= (count clean-number) 10) clean-number
+      (= (count clean-number) 9) invalid
       (= (first clean-number) \1) (string/join (rest clean-number))
-      :else "0000000000")))
+      :else invalid)))
 
 
 
 (defn area-code [n]
   (subs (number n) 0 3))
 
-(defn pretty-print [n])
+(defn pretty-print
+  "(123) 456-7890"
+  [n]
+  (let [n (number n)]
+    (str "(" (.substring n 0 3) ") " (.substring n 3 6) "-" (.substring n 6 10))))
