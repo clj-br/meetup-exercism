6544119b214c68e688137611a37e9b3196a9d09e
Tomás Augusto Müller <tomas.am@gmail.com>
2016-04-27 19:21:45 -0300

    Continue phone number exercise.

--- phone-number/src/phone_number.clj
+++ phone-number/src/phone_number.clj
@@ -2,9 +2,13 @@
   (:require [clojure.string :as string]))
 
 (defn number [n]
-  (cond
-    (= (count n) 10) (string/join (re-seq #"\d" n))
-    :else "0000000000"))
+  (let [clean-number (string/join (re-seq #"\d" n))]
+    (cond
+      (= (count clean-number) 10) clean-number
+      (= (first clean-number) \1) (string/join (rest clean-number))
+      :else "0000000000")))
+
+
 
 (defn area-code [n]
   (subs (number n) 0 3))
