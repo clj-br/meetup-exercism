63dc0c220df2ff368343f4a2bbf35fe1ed0785e7
Tomás Augusto Müller <tomas.am@gmail.com>
2016-05-04 19:30:32 -0300

    Solve robot name exercise.

--- robot-name/src/robot_name.clj
+++ robot-name/src/robot_name.clj
@@ -3,8 +3,13 @@
 (def alphabet "ABCDEFGHIJKLMNOPQRSQTUWZYZ")
 
 (defn robot []
-  (atom (format "%c%c%03d" (rand-nth alphabet) (rand-nth alphabet) (rand-int 1000))))
+  (atom (format "%c%c%03d"
+                (rand-nth alphabet)
+                (rand-nth alphabet)
+                (rand-int 1000))))
 
-(defn robot-name [robot])
+(defn robot-name [r]
+  @r)
 
-(defn reset-name [robot])
+(defn reset-name [r]
+  (reset! r (robot-name (robot))))
