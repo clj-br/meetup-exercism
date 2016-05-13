f3cc663b9e08d4f1d37574b969b566ba6bd739b1
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2016-05-12 21:11:16 -0300

    binary-search: search-for-pos extracted back to separate fn because it is not public.

--- binary-search/src/binary_search.clj
+++ binary-search/src/binary_search.clj
@@ -3,14 +3,13 @@
 (defn middle [coll]
   (quot (count coll) 2))
 
-(defn search-for
-  ([target coll]
-   (search-for target (vec coll) 0))
+(defn- search-for-pos [target coll position]
+  (assert (not-empty coll) "element not found")
+  (let [middle (middle coll)
+        found (coll middle)]
+    (cond (= found target) (+ position middle)
+          (> found target) (recur target (subvec coll 0 middle) position)
+          (< found target) (recur target (subvec coll (inc middle)) (+ position middle 1)))))
 
-  ([target coll position]
-    (assert (not-empty coll) "element not found")
-    (let [middle (middle coll)
-          found (coll middle)]
-      (cond (= found target) (+ position middle)
-            (> found target) (recur target (subvec coll 0 middle) position)
-            (< found target) (recur target (subvec coll (inc middle)) (+ position middle 1))))))
+(defn search-for [target coll]
+  (search-for-pos target (vec coll) 0))
