3996056970539f2550a995d6a54b8df24933d063
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2016-05-12 21:07:55 -0300

    binary-search: refactored to use two arities.

--- binary-search/src/binary_search.clj
+++ binary-search/src/binary_search.clj
@@ -3,15 +3,14 @@
 (defn middle [coll]
   (quot (count coll) 2))
 
-(defn- search-for-pos [target coll position]
-  (assert (not-empty coll) "element not found")
+(defn search-for
+  ([target coll]
+   (search-for target (vec coll) 0))
 
-  (let [middle (middle coll)
-        found (coll middle)]
-
-    (cond (= found target) (+ position middle)
-           (> found target) (search-for-pos target (subvec coll 0 middle) position)
-           (< found target) (search-for-pos target (subvec coll (inc middle)) (+ position middle 1)))))
-
-(defn search-for [target coll]
-  (search-for-pos target (vec coll) 0))
+  ([target coll position]
+    (assert (not-empty coll) "element not found")
+    (let [middle (middle coll)
+          found (coll middle)]
+      (cond (= found target) (+ position middle)
+            (> found target) (search-for target (subvec coll 0 middle) position)
+            (< found target) (search-for target (subvec coll (inc middle)) (+ position middle 1))))))
