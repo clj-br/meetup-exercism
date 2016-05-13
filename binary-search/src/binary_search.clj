bfb5da942d60b39dd9aa18df86b99e605a1bee07
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2016-05-12 21:03:10 -0300

    binary-search refactored

--- binary-search/src/binary_search.clj
+++ binary-search/src/binary_search.clj
@@ -6,16 +6,13 @@
 (defn- search-for-pos [target coll position]
   (when (empty? coll)
     (throw (ex-info "element not found" {:coll coll :target target})))
+
   (let [middle (middle coll)
         found (coll middle)]
-    (if
-      (and
-        (> position 1)
-        (= middle 0))
-      (throw (ex-info "element not found" {:coll coll :target target})))
+
     (cond (= found target) (+ position middle)
            (> found target) (search-for-pos target (subvec coll 0 middle) position)
-           (< found target) (search-for-pos target (subvec coll middle (count coll)) (+ position middle)))))
+           (< found target) (search-for-pos target (subvec coll (inc middle)) (+ position middle 1)))))
 
 (defn search-for [target coll]
   (search-for-pos target (vec coll) 0))
