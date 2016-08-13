5dca2dd5b5bd189ba9b7e3f20a39d8fc58fabff7
Andrew Rosa <andrewhr@me.com>
2016-08-13 14:05:06 -0300

    A little more progress (we hope)

--- meetup/src/meetup.clj
+++ meetup/src/meetup.clj
@@ -1,16 +1,25 @@
-(ns meetup)
+(ns meetup
+  (:import [java.util Calendar GregorianCalendar]))
 
 (defn meetup [month year week-day x]
-  (let [calendar (java.util.GregorianCalendar.)]
-    (.setWeekDate calendar year 10 1)
-    calendar))
+  (let [calendar (GregorianCalendar.)]
+    (doto calendar
+      (.set Calendar/YEAR year)
+      (.set Calendar/MONTH month))
+    [(.get calendar Calendar/YEAR)
+     (.get calendar Calendar/MONTH)
+     (.get calendar Calendar/DAY_OF_MONTH)]))
 
 (comment
 
   (meetup 2 2013 :friday :first)
 
 
-  (java.util.GregorianCalendar.)
+  (doto (GregorianCalendar.)
+    (.set Calendar/YEAR 2013)
+    (.set Calendar/MONTH 10)
+    (.set Calendar/DAY_OF_WEEK Calendar/MONDAY)
+    )
 
 
 
